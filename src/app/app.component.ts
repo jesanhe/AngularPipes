5956f1448bdb5bd829a9bb70f5642f2af78b643b
Jerónimo Sancho <jsanchoh@capgemini.com>
2019-01-31 12:56:36 +0100

    First push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Seccion5';
+
+  nombre = 'Jero';
+
+  array = [1, 2, 3, 4, 5, 7, 8, 9, 10];
+
+  pi = Math.PI;
+
+  a = 0.234;
 }
